88013ba78aa8cf911311c6753407089b4156b8ad
zhengyunan <2802493252@qq.com>
2018-10-11 21:49:05 +0800

    '文章分类'

--- controllers/Article_categoryController.php
+++ controllers/Article_categoryController.php
@@ -1,42 +1,38 @@
 <?php
 namespace controllers;
 
-use models\Article;
+use models\Article_category;
 
-class ArticleController{
+class Article_categoryController{
     // 列表页
     public function index()
     {
-        $model = new Article;
+        $model = new Article_category;
         $data = $model->findAll();
-        view('article/index', $data);
+        view('article_category/index', $data);
     }
 
     // 显示添加的表单
     public function create()
-    {   
-        // 取出分类的数据
-        $model = new \models\Article_category;
-        $data = $model->findAll();
-        view('article/create',$data);
-        // var_dump($data);die;
+    {
+        view('article_category/create');
     }
 
     // 处理添加表单
     public function insert()
     {
-        $model = new Article;
+        $model = new Article_category;
         $model->fill($_POST);
         $model->insert();
-        redirect('/article/index');
+        redirect('/article_category/index');
     }
 
     // 显示修改的表单
     public function edit()
     {
-        $model = new Article;
+        $model = new Article_category;
         $data=$model->findOne($_GET['id']);
-        view('article/edit', [
+        view('article_category/edit', [
             'data' => $data,    
         ]);
     }
@@ -44,17 +40,17 @@ class ArticleController{
     // 修改表单的方法
     public function update()
     {
-        $model = new Article;
+        $model = new Article_category;
         $model->fill($_POST);
         $model->update($_GET['id']);
-        redirect('/article/index');
+        redirect('/article_category/index');
     }
 
     // 删除
     public function delete()
     {
-        $model = new Article;
+        $model = new Article_category;
         $model->delete($_GET['id']);
-        redirect('/article/index');
+        redirect('/article_category/index');
     }
 }
